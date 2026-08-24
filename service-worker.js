84a381986abcb5d7fbd4286228946beb9185b9cc
maniselvaraj95 <maniselvarajsocial@gmail.com>
2026-08-24 19:12:25 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE="moneypulse-shell-v1";const ASSETS=["./","./index.html","./offline.html","./config.js","./css/styles.css","./js/app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-192.png","./icons/maskable-512.png","./icons/apple-touch-icon.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(e.request.method!=="GET"||u.origin!==self.location.origin)return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match("./offline.html"))));});
