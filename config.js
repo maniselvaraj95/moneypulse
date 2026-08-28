83299d4d0bc3ccf8da4a1fac90288c26810d0cfd
maniselvaraj95 <maniselvarajsocial@gmail.com>
2026-08-28 15:07:56 +0530

    Delete config.js

--- config.js
+++
@@ -1,1 +0,0 @@
-window.MONEY_PULSE_CONFIG=Object.freeze({GOOGLE_CLIENT_ID:"128124922822-7i1edunn60us4e7hhaia04iihp91q68f.apps.googleusercontent.com",APPS_SCRIPT_API_URL:"https://script.google.com/macros/s/AKfycbwLk1jPCZtq4_uxEkUHaL506VdVbxQET2_DeCAwlMcFwAuv8ZBTUFrjo2aQmZMoq9CC6Q/exec",API_VERSION:"1"});
